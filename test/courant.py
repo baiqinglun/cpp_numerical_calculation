920e9364393da0d243591eda781deb82ceee5446
baiqinglun <2398456523@qq.com>
2023-05-02 17:37:39 +0800

    方程离散

---
+++ test/courant.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+nx = 41
+dx=2/(nx-1)
+nt=20
+c=1
+sigma =0.5
+dt = sigma * dx
+u = np.ones(nx)
+u[int(0.5/dx):int(1/dx+1)]=2
+plt.plot(np.linspace (0,2,nx),u,'b',lw=3,label='origin')
+un=np.ones (nx)
+for n in range(nt):
+    un = u.copy()
+    for i in range(1,nx):
+        u[i] = un[i]-c*dt/dx*(un[i]-un[i-1])
+plt.plot(np.linspace(0,2,nx),u,'r',lw=3,label='current')
+plt.legend()
---
+++ test/main.cpp
@@ -0,0 +1,75 @@
+/**
+ * @brief: 方程离散案例1
+ * @birth: created by Dablelv on bql at 2023-05-02
+ */
+#include<iostream>
+
+using namespace std;
+
+class Differential 
+{
+private:
+    int N,
+        x_min,
+        x_max;
+    double dx,
+            *x,
+            *y;
+public:
+    Differential();
+    ~Differential();
+    void solution();
+    void function();
+};
+
+Differential::Differential()
+{
+    std::cout << "\n请输入网格个数:" << std::endl;
+    cin >> N;
+    std::cout << "\n请输入x最小值:" << std::endl;
+    cin >> x_min;
+    std::cout << "\n请输入x最大值:" << std::endl;
+    cin >> x_max;
+
+    dx = (x_max - x_min) / double(N) ;
+    std::cout << "\ndx = " << dx << std::endl;
+}
+
+Differential::~Differential()
+{
+    delete [] x;
+    delete [] y;
+}
+
+void Differential::solution()
+{
+    x = new double[N];
+    y = new double[N];
+   
+    for(int i = 0; i < N; ++i)
+    {
+        x[i] = i * dx;
+    };
+    for(int i = 0; i < N; ++i)
+    {
+        y[i] = 1.0;
+    };
+
+    for(int i = 0; i < N; ++i)
+    {
+        y[i+1] = y[i] + dx * (y[i] - 2*x[i]/y[i]);
+    };
+
+    cout << endl;
+    for(int i = 0; i < N; ++i)
+    {
+        std::cout << y[i] << "\t";
+    };
+}
+
+int main(){
+    Differential d;
+    d.solution();
+
+    return 0;
+}
